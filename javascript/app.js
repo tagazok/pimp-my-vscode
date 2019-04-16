92e6081cb7312e3652b966b0dc45ff6a8cbfbfc9
Olivier Leplus <olleplus@microsoft.com>
2019-04-17 00:18:37 +0200

    feat(bug): Add data2.json

--- javascript/app.js
+++ javascript/app.js
@@ -1,7 +1,7 @@
 fetchData();
 
 function fetchData() {
-    return fetch('/data/data.json')
+    return fetch('/data/data2.json')
         .then(function(response) {
             return response.json();
         }).then(function(data) {
